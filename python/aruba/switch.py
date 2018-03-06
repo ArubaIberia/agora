54f5887f8c2c921997b8c297d61ba73b4d8a8af7
Rafael Rivero <rafael_rivero@Riveros-MacBook-Pro.local>
2018-03-06 08:57:57 +0100

    Soporte para especificar la version de la api del switch

--- python/aruba/switch.py
+++ python/aruba/switch.py
@@ -9,17 +9,17 @@ from aruba.common import _ask_input, _ask_pass, _fill, Config, Session
 from contextlib import contextmanager
 
 
-def _api_url(api_host):
-    return "https://{}/rest/v3".format(api_host)
+def _api_url(api_host, api_version):
+    return "https://{}/rest/{}".format(api_host, api_version)
 
 # ------------------------
 # Métodos de autenticación
 # ------------------------
 
 
-def _login(api_host, username, password, verify=True):
+def _login(api_host, api_version, username, password, verify=True):
     """Lanza un intento de autenticación contra un switch, devuelve la cookie"""
-    login_url = _api_url(api_host) + "/login-sessions"
+    login_url = _api_url(api_host, api_version) + "/login-sessions"
     credentials = { "userName": username, "password": password }
     response = requests.post(login_url, verify=verify, json=credentials)
     if response.status_code != 201:
@@ -32,9 +32,9 @@ def _login(api_host, username, password, verify=True):
     return cookie
 
 
-def _logout(api_host, cookie, verify=True):
+def _logout(api_host, api_version, cookie, verify=True):
     """Cierra una sesion REST contra un switch"""
-    logout_url = _api_url(api_host) + "/login-sessions"
+    logout_url = _api_url(api_host, api_version) + "/login-sessions"
     headers = { "Cookie": cookie }
     response = requests.delete(logout_url, verify=verify, headers=headers)
     if response.status_code != 204:
@@ -42,26 +42,28 @@ def _logout(api_host, cookie, verify=True):
 
 
 @contextmanager
-def session(config, api_host=None, username=None, password=None, verify=True):
+def session(config, api_host=None, api_version=None, username=None, password=None, verify=True):
     """Obtiene una cookie para un switch"""
     # Cargo de la config valores por defecto para todos los parámetros
     data = _fill({
         "api_host": api_host,
         "username": username,
         "password": password,
+        "api_version": api_version,
     }, config.get("switch"))
     # Saco las variables del array, por comodidad
-    api_host, username, password = (
+    api_host, username, password, api_version = (
         data["api_host"],
         data["username"],
         data["password"],
+        data["api_version"]
     )
     # Lanzo la autenticacion que corresponda
-    cookie = _login(api_host, username, password, verify=verify)
+    cookie = _login(api_host, api_version, username, password, verify=verify)
     try:
-        yield Session(_api_url(api_host), cookie, headers={ "Cookie": cookie })
+        yield Session(_api_url(api_host, api_version), cookie, headers={ "Cookie": cookie })
     finally:
-        _logout(api_host, cookie, verify=verify)
+        _logout(api_host, api_version, cookie, verify=verify)
 
 
 if __name__ == "__main__":
@@ -70,7 +72,8 @@ if __name__ == "__main__":
     defaults = {
         "api_host": None,
         "username": None,
-        "password": None
+        "password": None,
+        "api_version": "v4",
     }
     config = Config()
     defaults.update(config.get("switch"))
@@ -81,14 +84,16 @@ if __name__ == "__main__":
         "api_host": _ask_input("Nombre del servidor", defaults["api_host"]),
         "username": _ask_input("Nombre de usuario", defaults["username"]),
         "password": _ask_pass("Password"),
+        "api_version": _ask_input("Version de la API", defaults["api_version"]),
     }
 
     # Pruebo la autenticación y giardo la config
-    cookie = _login(data["api_host"], data["username"], data["password"], verify=False)
+    cookie = _login(data["api_host"], data["api_version"], data["username"], data["password"], verify=False)
     try:
         print("OK! cookie = %s" % cookie)
         config.set("switch", data)
         config.save()
         print("Tokens guardados en %s" % config.path())
     finally:
-        _logout(data["api_host"], cookie, verify=False)
+        _logout(data["api_host"], data["api_version"], cookie, verify=False)
+
