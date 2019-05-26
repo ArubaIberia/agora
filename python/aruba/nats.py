283fa913d04316f398aaafe5bdc75ad0c37309cb
rjrivero <private@email.com>
2019-05-26 14:23:29 +0200

    Added NATS App

--- python/aruba/nats.py
+++ python/aruba/nats.py
@@ -6,8 +6,8 @@ import traceback
 import asyncio
 import aiohttp
 
+from contextlib import asynccontextmanager, AsyncExitStack
 from nats.aio.client import Client as NATS
-from contextlib import asynccontextmanager
 
 
 class Suscription(object):
@@ -87,3 +87,86 @@ async def suscription(natsURL, loop):
     logging.debug("Suscription - Connected to NATS server")
     yield Suscription(natsConn, loop)
     natsConn.close()
+
+
+class App():
+
+    """Message-based Application.
+
+    Manages a Session and a NATS connection, to subscribe to
+    NATS topics and trigger actions on the session.
+    """
+
+    def __init__(self, natsURL, contextCallback, verify=True):
+        """Build an app connected to the given NATS server.
+        
+        contextCallback must be a function that takes no arguments and returns a
+        contextmanager, e.g:
+
+        Works:
+        >>> app = App(myURL, lambda: clearpass.session(config))
+
+        Does not work:
+        >>> app = App(myURL, clearpass.session(config))
+        """
+        self._loop = asyncio.get_event_loop()
+        self._natsURL = natsURL
+        self._context = contextCallback
+        self._verify = verify
+        self.prodSession = None
+        self.httpSession = None
+        self.natsSession = None
+
+    async def start(self):
+        "Start the app. This is not reentrant, an only be started once."
+        logging.debug("Iniciando proceso principal")
+        async with AsyncExitStack() as stack:
+            prodSession = stack.enter_context(self._context())
+            logging.debug("Iniciado contexto de aplicación")
+            httpSession = await stack.enter_async_context(aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=self._verify)))
+            logging.debug("Iniciado pool HTTP")
+            natsSession = await stack.enter_async_context(suscription(self.natsURL, self.loop))
+            logging.debug("Lanzada conexion a NATS URL")
+            self.prodSession = prodSession
+            self.httpSession = httpSession
+            self.natsSession = natsSession
+            # If session supports refresh, refresh it.
+            if hasattr(prodSession, "refresh"):
+                await stack.enter_async_context(natsSession.refresh(prodSession.refresh, 7200))
+                logging.debug("Lanzado proceso de refresco de sesion")
+            self._stop = stack.pop_all().aclose
+            self._topics = dict()
+
+    async def subscribe(self, topic, asyncCallback):
+        """Subscribe a topic. asyncCallback will be called with topic name, and message.
+
+        Caution: asyncCallback must be an asyncfunction, not a lambda. E.g. this doesn't work:
+
+        >>> app.subscribe("topic", (lambda topic, msg: doStuff()))
+
+        Instead you mst create an async callback function:
+
+        >>> async def myCallback(topic, msg):
+        >>>     return await doStuff()
+        >>> app.subscribe("topic", myCallback)
+        """
+        async with AsyncExitStack() as stack:
+            await stack.enter_async_context(self.natsSession.topic(topic, asyncCallback))
+            logging.debug("Suscrito a tópico {}, esperando mensajes...".format(topic))
+            self._topics[topic] = stack.pop_all().aclose
+
+    async def stop(self):
+        "Stop the nat connection and all subscritions. Call with await"
+        for closeFunc in self._topics.values():
+            await closeFunc()
+        self._topics = dict()
+        if self._stop is not None:
+            await self._stop()
+        self._stop = None
+
+    async def unsubscribe(self, topic):
+        "Unsubscribe from the topic. Call with await"
+        closeFunc = self._topics.get(topic, None)
+        if closeFunc is not None:
+            await closeFunc()
+            del self._topics[topic]
